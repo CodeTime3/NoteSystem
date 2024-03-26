9d6cb82e920cf3987337855755facaa0a73750e4
CodeTime3 <daniele.pilloni.03@gmail.com>
2024-03-26 21:25:29 +0100

    init token crud

---
+++ NoteSystemDao/TokenCrud.cs
@@ -0,0 +1,18 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace NoteSystemDao
+{
+    public class TokenCrud
+    {
+        private readonly MySqlConnection _connection;
+        public TokenCrud(string connectionStr)
+        {
+            _connection = new MySqlConnection(connectionStr);
+        }
+
+        public int AddNewItem()
+        {
+            return 1;
+        }
+    }
+}
