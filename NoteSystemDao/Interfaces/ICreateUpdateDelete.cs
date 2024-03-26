e45ba54fd59c61f7032e067ae9f0aebd060b63e2
CodeTime3 <daniele.pilloni.03@gmail.com>
2024-03-26 21:24:38 +0100

    little refactoring

---
+++ NoteSystemDao/Interfaces/ICreateUpdateDelete.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoteSystemDao.Interfaces
+{
+    public interface ICreateUpdateDelete<T>
+    {
+        int AddNewItem(T item);
+
+        int UpdateItem(T item);
+
+        int DeleteItem(int id);
+    }
+}
---
+++ NoteSystemDao/Interfaces/ISelectItems.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoteSystemDao.Interfaces
+{
+    public interface ISelectItems<T>
+    {
+        T[] GetAllItems();
+
+        T[] GetItems(string? str);
+    }
+}
