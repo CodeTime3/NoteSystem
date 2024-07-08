c08ea24f1e5e9f6d77d46c95b74879431e9d92a8
CodeTime3 <daniele.pilloni.03@gmail.com>
2024-07-08 16:08:34 +0200

    test on the last method

--- NoteSystemTest/GeneralUserCrudTest.cs
+++ NoteSystemTest/GeneralUserCrudTest.cs
@@ -4,7 +4,7 @@
     {
         private static readonly string _connectionString = "server=localhost;uid=root;pwd=CT22d03p06;database=notesystem";
         private readonly MySqlConnection _connection = new MySqlConnection(_connectionString);
-        private GeneralCrud<UserDbItem> _generalCrud;
+        private GeneralCrud _generalCrud;
         private UserDbItem _user = new UserDbItem { UserName = "pluto", UserHash = "pippo" };
 
         [Fact]
@@ -13,7 +13,7 @@
             using (var connection = _connection)
             {
                 connection.Open();
-                _generalCrud = new GeneralCrud<UserDbItem>(connection);
+                _generalCrud = new GeneralCrud(connection);
                 int result = _generalCrud.CreateItem(_user);
 
                 Assert.Equal(1, result);
@@ -27,10 +27,26 @@
             {
                 connection.Open();
                 UserDbItem[] users = new UserDbItem[30];
-                _generalCrud = new GeneralCrud<UserDbItem>(connection);
+                _generalCrud = new GeneralCrud(connection);
                 users = _generalCrud.ReadAllItems(_user);
 
-                Assert.Equal(8, users.Length);
+                Assert.Equal(7, users.Length);
+            }
+        }
+
+        [Fact]
+        public void GetItem_should_work()
+        {
+            using (var connection = _connection)
+            {
+                connection.Open();
+                UserDbItem user = new UserDbItem();
+                List<UserDbItem> users = new List<UserDbItem>();
+                _generalCrud = new GeneralCrud(connection);
+                user = _generalCrud.GetItem(_user, "pippo");
+                users.Add(user); 
+
+                Assert.Equal(1, users.Count);
             }
         }
 
@@ -40,7 +56,7 @@
             using (var connection = _connection)
             {
                 connection.Open();
-                _generalCrud = new GeneralCrud<UserDbItem>(connection);
+                _generalCrud = new GeneralCrud(connection);
                 int result = _generalCrud.UpdateItem(_user, 11);
 
                 Assert.Equal(1, result);
@@ -53,7 +69,7 @@
             using (var connection = _connection)
             {
                 connection.Open();
-                _generalCrud = new GeneralCrud<UserDbItem>(connection);
+                _generalCrud = new GeneralCrud(connection);
                 int result = _generalCrud.DeleteItem(_user, 11);
 
                 Assert.Equal(1, result);
