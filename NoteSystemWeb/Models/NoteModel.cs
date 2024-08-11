180b7995d612210c3a6be5d92644bcb7a2d5d626
CodeTime3 <daniele.pilloni.03@gmail.com>
2024-08-11 19:48:28 +0200

    added a model for the home

---
+++ NoteSystemWeb/Models/NoteModel.cs
@@ -0,0 +1,8 @@
+﻿namespace NoteSystemWeb.Models
+{
+    public class NoteModel
+    {
+        public string NoteTitle { get; set; } = string.Empty;
+        public string NoteText { get; set; } = string.Empty;
+    }
+}
