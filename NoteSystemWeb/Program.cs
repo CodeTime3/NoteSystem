7b52d39693e25a3bba303df812a06c988b2841a8
CodeTime3 <daniele.pilloni.03@gmail.com>
2024-08-11 19:47:51 +0200

    added singleton for the crud and init authentication

--- NoteSystemWeb/Program.cs
+++ NoteSystemWeb/Program.cs
@@ -1,13 +1,29 @@
+using MySql.Data.MySqlClient;
+
 namespace NoteSystemWeb
 {
     public class Program
     {
         public static void Main(string[] args)
-        {
+        {   
             var builder = WebApplication.CreateBuilder(args);
+            string connectionString = "server=localhost;uid=root;pwd=CT22d03p06;database=notesystem";
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            GeneralCrud generalCrud = new GeneralCrud(connection);
 
+            builder.Services.AddSingleton(generalCrud);
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).
+                AddCookie(
+                    options => 
+                    {
+                        options.ExpireTimeSpan = TimeSpan.FromMinutes(20);
+                        options.SlidingExpiration = true;
+                        options.AccessDeniedPath = "/Forbidden/";
+                    }
+                );
 
             var app = builder.Build();
 
@@ -24,11 +40,13 @@ namespace NoteSystemWeb
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
+            app.MapRazorPages();
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+                pattern: "{controller=Account}/{action=Login}/{id?}");
 
             app.Run();
         }
---
+++ NoteSystemWeb/Usings.cs
@@ -0,0 +1,7 @@
+﻿global using NoteSystemDao.DbItems;
+global using NoteSystemDao.Orm;
+global using Microsoft.AspNetCore.Mvc;
+global using NoteSystemWeb.Models;
+global using System.Diagnostics;
+global using Microsoft.AspNetCore.Identity;
+global using Microsoft.AspNetCore.Authentication.Cookies;
