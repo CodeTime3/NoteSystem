ccbcdded5af2482670617a3c5eb5b5e71bfa0d56
CodeTime3 <daniele.pilloni.03@gmail.com>
2024-08-17 23:10:24 +0200

    init controller in the HomeController for the system note

--- NoteSystemWeb/Controllers/HomeController.cs
+++ NoteSystemWeb/Controllers/HomeController.cs
@@ -2,9 +2,39 @@ namespace NoteSystemWeb.Controllers
 {
     public class HomeController(GeneralCrud crud) : Controller
     {   
+        [HttpPost]
+        public IActionResult CreateNote(NoteModel noteModel)
+        {   
+            noteModel.UserId = 3;
+            crud.CreateItem(noteModel);
+
+            return RedirectToAction("Index", "Home");
+        }
+ 
+        [HttpPost]
+        public IActionResult UpdateNote(NoteModel noteModel)
+        {   
+            noteModel.UserId = 3;
+            crud.UpdateItem(noteModel, noteModel.UserId);
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        public IActionResult CreateNote()
+        {
+            var model = new NoteModel();
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult UpdateNote()
+        {   
+            var model = new NoteModel();
+            return View(model);
+        }
+
         public IActionResult Index()
         {
-            List<NoteDbItem> list = new List<NoteDbItem>();
             return View();
         }
     }
