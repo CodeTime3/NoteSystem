862ef2974d27cb1e2de93b4f9b6fa29c298c2cfa
CodeTime3 <daniele.pilloni.03@gmail.com>
2024-09-27 13:09:39 +0200

    did some tests and added a test for the read method

--- NoteSystemTest/GeneralUserCrudTest.cs
+++ NoteSystemTest/GeneralUserCrudTest.cs
@@ -5,16 +5,17 @@
         private static readonly string _connectionString = "server=localhost;uid=root;pwd=CT22d03p06;database=notesystem";
         private readonly MySqlConnection _connection = new MySqlConnection(_connectionString);
         private GeneralCrud _generalCrud;
-        private UserDbItem _user = new UserDbItem { UserName = "pluto25", UserHash = "pippo25" };
+        private UserDbItem _user = new UserDbItem { UserName = "pluto2", UserHash = "pippo25" };
+        private NoteDbItem _note = new NoteDbItem { UserId = 3, NoteText = "h", NoteTitle = "h" };
 
         [Fact]
         public void CreateItem_should_work()
         {
             using (var connection = _connection)
-            {
+            {   
                 connection.Open();
                 _generalCrud = new GeneralCrud(connection);
-                int result = _generalCrud.CreateItem(_user);
+                int result = _generalCrud.CreateItem(_note);
 
                 Assert.Equal(1, result);
             }
@@ -51,6 +52,22 @@
         }
 
         [Fact]
+        public void GetItem_should_work2()
+        {
+            using (var connection = _connection)
+            {
+                connection.Open();
+                NoteDbItem note = new NoteDbItem();
+                List<NoteDbItem> notes = new List<NoteDbItem>();
+                _generalCrud = new GeneralCrud(connection);
+                note = _generalCrud.ReadItem(note, 5);
+                notes.Add(note); 
+
+                Assert.Equal(1, notes.Count);
+            }
+        }
+
+        [Fact]
         public void ReadItems_should_work()
         {
             using (var connection = _connection)
@@ -69,10 +86,11 @@
         public void UpdateItem_should_work()
         {
             using (var connection = _connection)
-            {
+            {   
+                NoteDbItem note = new NoteDbItem(5, "danihh", null);
                 connection.Open();
                 _generalCrud = new GeneralCrud(connection);
-                int result = _generalCrud.UpdateItem(_user, 11);
+                int result = _generalCrud.UpdateItem(note, 13);
 
                 Assert.Equal(1, result);
             }
