748e4f83bc48014e34e473cc95c1e2351b1f1af8
CodeTime3 <daniele.pilloni.03@gmail.com>
2024-09-27 12:59:54 +0200

    modified the note model for the home controller

--- NoteSystemWeb/Models/NoteModel.cs
+++ NoteSystemWeb/Models/NoteModel.cs
@@ -2,8 +2,6 @@
 {
     public class NoteModel
     {
-        public int UserId { get; set; } 
-        public string NoteTitle { get; set; } = string.Empty;
-        public string NoteText { get; set; } = string.Empty;
+        public NoteDbItem[] Notes { get; set; } = [];
     }
 }
