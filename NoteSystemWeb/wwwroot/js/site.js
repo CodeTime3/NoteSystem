1ec80d8aaa385dc7f068bdc45f34633679bd8fd0
CodeTime3 <daniele.pilloni.03@gmail.com>
2024-10-13 20:48:42 +0200

    added an event in the navbar for the user icon

--- NoteSystemWeb/wwwroot/js/site.js
+++ NoteSystemWeb/wwwroot/js/site.js
@@ -1,4 +1,16 @@
 ﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+const userBtn = document.getElementById('user-btn');
+const navbar = document.getElementById('navbar');
+
+userBtn.addEventListener('click', function(){
+
+    if(navbar.classList[1] == "sidebar"){
+
+        navbar.classList.remove('sidebar');
+    }else{
+
+        navbar.classList.add('sidebar');
+    }
+});
