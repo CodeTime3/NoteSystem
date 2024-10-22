6ad7e8eff1005b19e09a60b5d2db1abc2f717d8f
CodeTime3 <daniele.pilloni.03@gmail.com>
2024-10-22 18:11:08 +0200

    deleted useSession and changed the lifetimes for services in DI

--- NoteSystemWeb/Program.cs
+++ NoteSystemWeb/Program.cs
@@ -5,16 +5,15 @@ namespace NoteSystemWeb
         public static void Main(string[] args)
         {   
             var builder = WebApplication.CreateBuilder(args);
-            string connectionString = "server=localhost;uid=root;pwd=CT22d03p06;database=notesystem";
+            string connectionString = builder.Configuration.GetConnectionString("MyDB");
             MySqlConnection connection = new MySqlConnection(connectionString);
             GeneralCrud generalCrud = new GeneralCrud(connection);
 
-            builder.Services.AddSingleton(generalCrud);
+            builder.Services.AddScoped(sp => generalCrud);
             // Add services to the container.
             builder.Services.AddControllersWithViews();
 
 			builder.Services.AddDistributedMemoryCache();
-			builder.Services.AddSession();
 
 			builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).
                 AddCookie(
@@ -22,7 +21,6 @@ namespace NoteSystemWeb
                     {
                         options.ExpireTimeSpan = TimeSpan.FromMinutes(20);
                         options.SlidingExpiration = true;
-                        options.AccessDeniedPath = "/Forbidden/";
                     }
                 );
 
@@ -40,7 +38,6 @@ namespace NoteSystemWeb
             app.UseStaticFiles();
 
             app.UseRouting();
-            app.UseSession();
             
             app.UseAuthentication();
 
